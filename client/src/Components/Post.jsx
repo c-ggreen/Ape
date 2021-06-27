5c7d164e0058a49d4dceea193dbef8c99ab1496d
Chadwick Green <chadggreen@gmail.com>
2021-06-27 15:03:50 -0400

    update post functionality and modal

--- client/src/Components/Post.jsx
+++ client/src/Components/Post.jsx
@@ -1,8 +1,32 @@
-import React from "react";
-import { Card, Container, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Container, Button, Modal, Form } from "react-bootstrap";
 import PostService from "../Services/PostService";
 
 function Post(props) {
+  // For the "Edit" modal.
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // For the "editPost" functionality
+  const [newTopic, setNewTopic] = useState("");
+  const [newName, setNewName] = useState("");
+  const [newPost, setNewPost] = useState("");
+
+  // Handles the new input for Topic
+  const handleNewTopic = (e) => {
+    setNewTopic(e.target.value);
+  };
+  // Handles the new input for Name
+  const handleNewName = (e) => {
+    setNewName(e.target.value);
+  };
+
+  // Handles the new input for Post
+  const handleNewPost = (e) => {
+    setNewPost(e.target.value);
+  };
+
   // Function responsible for deleting a post
   const deletePost = (id) => {
     PostService.deletePost(id).then((res) => {
@@ -12,7 +36,21 @@ function Post(props) {
     });
   };
 
-  const editPost = () => {};
+  // Function responsible for editing/updating a post
+  // Note: for some reason it doesn't like when I add event.preventDefault() to it, and works just fine without it.
+  // Note: also this only works if I disable the CORS chrome extension.
+  const editPost = () => {
+    PostService.patchPost({
+      id: props.id,
+      topic: newTopic,
+      name: newName,
+      post: newPost,
+    }).then((res) => {
+      console.log(res.data);
+      props.getPosts();
+      handleClose();
+    });
+  };
   return (
     <div>
       <br />
@@ -25,23 +63,76 @@ function Post(props) {
               <footer className="blockquote-footer">{props.name}</footer>
             </blockquote>
           </Card.Body>
-          <Button
-            onClick={() => deletePost(props.id)}
-            variant="danger"
-            style={{ width: "10%" }}
-          >
-            Delete
-          </Button>
-          <Button
-            onClick={() => editPost()}
-            variant="secondary"
-            style={{ width: "10%" }}
-          >
-            Edit
-          </Button>
+          <Card.Footer>
+            <Button
+              onClick={() => deletePost(props.id)}
+              variant="danger"
+              style={{ width: "10%" }}
+            >
+              Delete
+            </Button>
+            <Button
+              onClick={() => handleShow()}
+              variant="secondary"
+              style={{ width: "10%" }}
+            >
+              Edit
+            </Button>
+          </Card.Footer>
         </Card>
       </Container>
       <br />
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Post</Modal.Title>
+        </Modal.Header>
+        <Container>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Topic</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder={props.topic}
+                onChange={handleNewTopic}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder={props.name}
+                onChange={handleNewName}
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Make a post</Form.Label>
+              <Form.Control
+                as="textarea"
+                placeholder={props.post}
+                rows={3}
+                onChange={handleNewPost}
+              />
+            </Form.Group>
+          </Form>
+        </Container>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="warning" onClick={() => editPost()}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
