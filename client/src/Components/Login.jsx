7601e8917a3cb2e0837f156128338837a167c54e
Chadwick Green <chadggreen@gmail.com>
2021-06-28 23:16:35 -0400

    title screen styling

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -21,30 +21,29 @@ function Login() {
 
   return (
     <div className="containerAll">
-      <Container>
-        <Row>
-          <Col xs={4}>
-            <h1 className="h3 mb-3 font-size-normal">APE</h1>
-            <Link to="/home">
-              <Button variant="warning" type="submit">
-                Enter
-              </Button>
-            </Link>
-          </Col>
+      <Col
+        xs={6}
+        className="d-flex flex-column justify-content-center align-items-center"
+      >
+        <h1 className="h1 mb-3 font-size-normal">APE</h1>
+        <Link to="/home">
+          <Button variant="warning" type="submit">
+            Enter
+          </Button>
+        </Link>
+      </Col>
 
-          <Col xs={8}>
-            {/* Quotes should go here */}
-            {quotes.slice(quoteNum, quoteNum + 1).map((item, i) => {
-              return (
-                <div key={i} className="quotes">
-                  <h1>"{item.quote}"</h1>
-                  <h5>- {item.source}</h5>
-                </div>
-              );
-            })}
-          </Col>
-        </Row>
-      </Container>
+      <Col xs={6}>
+        {/* Quotes should go here */}
+        {quotes.slice(quoteNum, quoteNum + 1).map((item, i) => {
+          return (
+            <div key={i} className="quotes">
+              <h1>"{item.quote}"</h1>
+              <h5>- {item.source}</h5>
+            </div>
+          );
+        })}
+      </Col>
     </div>
   );
 }
